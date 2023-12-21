08f689cb10159544a4b4ebf687704b13654d9bbb
Nemesai38 <pappysouls@gmail.com>
2023-12-21 14:24:15 +0100

    Sehr gut! completed on first trial

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let myArray = word.split("");
+    let newArray = [];
+    let len = word.length;
+    for (i = 0; i < len; i++) {
+        newArray.unshift(myArray[i]);
+    }
+    let newword = newArray.join("");
+    return newword;
 };
 
 // Do not edit below this line
