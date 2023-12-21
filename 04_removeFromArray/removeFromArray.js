2d0fc32c8b2fbd0bade42ef8ac2753291f35f551
Nemesai38 <pappysouls@gmail.com>
2023-12-21 18:51:17 +0100

    exercise completed successfully, Sehr gut!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(args) {
+    let argArraylen = arguments.length;
+    for (let i = 1; i < argArraylen; i++) {
+        let realArg = arguments[i];
+        let realArgIndex = arguments[0].indexOf(realArg);
+        if (realArgIndex === -1) {
+            continue
+        }
+        arguments[0].splice(realArgIndex, 1)
+    }
+    return arguments[0];
 };
 
 // Do not edit below this line
