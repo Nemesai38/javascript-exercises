7c817d1f68732115bba5bbf7ae1ba28f166ced14
Nemesai38 <pappysouls@gmail.com>
2023-12-22 12:56:44 +0100

    completed. Result very surprising!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (isNaN(int1) || isNaN(int2)) return "ERROR";
+    if (typeof int1 === "string" || typeof int2 === "string") return "ERROR"
+    if (int1 < 0 || int2 < 0) return "ERROR";
+    if (int1 > int2) {
+        let int2 = arguments[0];
+        let int1 = arguments[1];
+        let array = [int1];
+        for (let i = int1; i < int2; i++) {
+            int1 += 1;
+            array.push(int1)
+        }
+        let arraysum = array[0];
+        for (let i = 1; i < array.length; i++) {
+            arraysum += array[i]
+        }
+        return arraysum;
+    } else {
+        let array = [int1];
+        for (let i = int1; i < int2; i++) {
+            int1 += 1;
+            array.push(int1);
+        }
+        let arraysum = array[0];
+        for (let i = 1; i < array.length; i++) {
+            arraysum += array[i]
+        }
+        return arraysum;
+    }
 };
 
 // Do not edit below this line
