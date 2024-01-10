b6e09e4c4c982e1299ae51d7ab57448446f2dc01
Nemesai38 <pappysouls@gmail.com>
2024-01-10 07:33:55 +0100

    calc, palindromes & fibonacci complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const sum = function (numbers) {
+  return numbers.reduce((total, number) => total + number, 0)
 
 };
 
-const power = function() {
-	
+const multiply = function (numbers) {
+  return numbers.reduce((first, second) => first * second);
 };
 
-const factorial = function() {
-	
+const power = function (a, b) {
+	return a**b
+};
+
+const factorial = function (a) {
+  if (a === 0) return 1;
+  let product = 1;
+  for (let i = a; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
