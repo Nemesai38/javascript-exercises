d211102c2c4c0542110e73a202369527c9c8ee3e
Nemesai38 <pappysouls@gmail.com>
2024-01-10 09:17:25 +0100

    final commit

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b) => {
+        const firstperson = a.yearOfDeath - b.yearOfBirth;
+        const nextperson = a.yearOfDeath - b.yearOfBirth;
+        return firstperson > nextperson ? 1 : -1;
+    })
 
+    return oldest[0];
 };
 
 // Do not edit below this line
